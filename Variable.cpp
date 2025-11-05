52e842bc257ce4f77e8fb38237bf1b347763e2cd
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-11-05 14:38:33 +0530

    just learned variables

---
+++ Variable.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    //integer(whole numbers)
+    int age =18;
+    int year = 2025;
+
+    //double(decimal number)
+    double price = 10.99;
+    double gpa = 5.67;
+
+    // single character
+    char grade = 'A';
+
+    //Boolean(true or false)
+    bool Student= 0;
+
+    //strings(objects that represents a sequence of text)
+    std::string name = "Himanshu";
+    std::string food = "pizza";
+    std::string day = "monday";
+    std::string address ="123 fake street";
+
+    std::cout << "Hello "<< name << std::endl;
+    std::cout << "You are "<< age << " Yrs old"<< std::endl;
+
+    return 0;
+}
