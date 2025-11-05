577b1ebcb8adaf8ff7bfc107bf7495b140f7e9fa
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-11-05 19:20:59 +0530

    just learnt constants

---
+++ Constants.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    const double PI = 3.14;
+    double radius = 30;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference;
+
+    return 0;
+}
