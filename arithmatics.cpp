e862c1b037f36f97d2c9f4f9ccb6c254013ecc5d
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-11-06 18:00:57 +0530

    just learned arithmetic operators

---
+++ arithmatics.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+// arithmatic operators = returns the result of a specific
+//                        arithmetic operations(+ - * /)
+
+
+int main()
+{
+    int students = 15;
+    
+    //students += 1;
+    //students++;
+
+    //students -= 2
+    //students--;
+    
+    //students *= 2;
+    
+    //students /= 5;
+
+    int remainder = students % 2;
+
+    std::cout << remainder ;
+
+    return 0;
+}
---
+++ arithmetics01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//perenthesis
+//multiplication & division
+//addition & subtraction
+
+int main()
+{
+    double students = 6 - 5 + 8 * 7 / 9;
+
+    std::cout << students;
+
+    return 0;
+}
